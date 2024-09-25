ad1eb8019614112b1b3345118687836a66c3ddf7
praveenpc1986 <praveenpc2006@gmail.com>
2024-09-25 11:22:58 +0800

    Comments

---
+++ src/sept/ex_09092024/Lab009.java
@@ -0,0 +1,19 @@
+package sept.ex_09092024;
+
+public class Lab009 {
+
+    public static void main(String[] args) {
+        // This is single line comment
+        // This will not be executed
+
+        /*
+         * This is multiline comments
+         *
+         * */
+        /**
+         * This is for java documentations
+         *
+         *
+        *  **/
+    }
+}
---
+++ src/sept/ex_09092024/Lab011.java
@@ -0,0 +1,10 @@
+package sept.ex_09092024;
+
+public class Lab011 {
+
+    public static void main(String[] args) {
+        // Variables and Data Types, Identifiers
+        boolean is_athi_marries=true;
+        
+    }
+}
