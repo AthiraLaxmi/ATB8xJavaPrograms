717725d71edcec28ed2e8649bbe9ba3b2a766a4b
praveenpc1986 <praveenpc2006@gmail.com>
2024-09-30 13:33:53 +0800

    All files 13 Sept

--- src/sept/Task/Task_02.java
+++ src/sept/Task/Task_02.java
@@ -1,15 +1,14 @@
-package sept.ex_13092024_Non_PrimitiveDT;
+package sept.Task;
 
-public class Lab027 {
+public class Task_02 {
     public static void main(String[] args) {
-        double a=34;
-        double b=10;
+        double a = 34;
+        double b = 10;
 
         System.out.println(" a + b =" + (a + b));
         System.out.println(" a - b =" + (a - b));
         System.out.println(" a * b =" + (a * b));
-        System.out.println(" a / b =" + (a/b));
-        System.out.println(" a % b =" + (a%b));
+        System.out.println(" a / b =" + (a / b));
+        System.out.println(" a % b =" + (a % b));
     }
-
 }
