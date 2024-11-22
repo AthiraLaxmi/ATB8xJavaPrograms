d35adaf3b996b5243c1819cfe28bf4ca1d8eeaa2
praveenpc1986 <praveenpc2006@gmail.com>
2024-11-22 17:13:14 +0800

    All Task 2nd Oct

---
+++ src/Oct/ex_02102024/ATBStudent.java
@@ -0,0 +1,22 @@
+package Oct.ex_02102024;
+
+public class ATBStudent {
+    // C - blueprint
+    //A - Attributes | Data members | instance variables
+    long phone;
+    String email;
+    int age;
+    int batch8x;
+    String name;
+    boolean isFemale;
+    String address;
+    boolean isFeePaid;
+    String githublink;
+    String SID;
+
+    // B- Behaviour | methods | member functions
+
+    void study() {}
+    void eat() {}
+    void walk() {}
+}
---
+++ src/Oct/ex_02102024/Lab108.java
@@ -0,0 +1,19 @@
+package Oct.ex_02102024;
+
+public class Lab108 {
+    public static void main(String[] args) {
+        //Class- Blueprint/template to create an object in the real life
+        // class can have some properties and functions
+        // the class is used in the main
+        // when a class is created no memory is allocated
+        // it contains attributes and behaviour
+        // class is a blueprint to create something and
+        // objects are the instance of the class created from the object
+
+        ATBStudent athira = new ATBStudent();
+        athira.age =35;
+
+        ATBStudent priya = new ATBStudent();
+        priya.age= 40;
+    }
+}
